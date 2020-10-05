4584d6dda8e712e6f9dcab5bcc909ff7bd6f01c3
Eduardo Maia <dudu_maia13@hotmail.com>
2020-10-05 18:11:30 -0400

    chore: camera e menu de pause

---
+++ Pixel Game/Assets/Scripts/GUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUI : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public Button pauseBtn, playBtn;
+    public static GUI instance;
+
+    void Awake ()
+    {
+        if(instance == null) instance = this;
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(this);
+
+        pauseScreen.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseBtn.onClick.AddListener(Pause);
+        playBtn.onClick.AddListener(Resume);
+    }
+
+    void Pause ()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void Resume ()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
--- Pixel Game/Assets/followPlayer.cs
+++ Pixel Game/Assets/followPlayer.cs
@@ -15,5 +15,17 @@ public class followPlayer : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(player.position.x,player.position.y, -10);
+        if(player.position.x >= -7.5 && player.position.x <= -1.5)
+        {
+            transform.position = new Vector3(0,0.32f,-10);
+        }
+        if(player.position.x <= -30)
+        {
+            transform.position = new Vector3(-30.85f,0.32f,-10);
+        }
+        if(player.position.x >= 63.27)
+        {
+            transform.position = new Vector3(63.27f, 1.32f, -10);
+        }
     }
 }
